98887f0492e881ecb35a87d0d5d59e5fb3e3df49
Joe Langston <joelangston81@gmail.com>
2020-04-28 16:12:15 -0500

    added html css and start of buttons

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+const resetButton = document.querySelector ( '#reset-button')
+
+const teamoneShootButton = document.querySelector('#teamone-shoot-button')
+
+const teamTwoShotButton = document.querySelector('#teamtwo-shoot-button')
+
+
+
+
+
+
+
+resetButton.addEventListener ('click', function(){
+
+
+
+})
+
+
+
+
+
+
+
+teamoneShootButton.addEventListener ('click', function(){
+
+
+
+
+
+})
+
+teamTwoShotButton.addEventListener ('click', function(){
+
+
+
+
+
+
+    
+})
