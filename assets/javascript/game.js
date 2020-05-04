d52babc1b465b595443a4f7a3c793761ceaba6e2
Joe Langston <joelangston81@gmail.com>
2020-05-04 14:58:12 -0500

    added random shot

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,14 @@ let counterTwo = document.querySelector('#teamtwo-numgoals');
 let shotOne = document.querySelector('#teamone-numshots');
 let counterOne = document.querySelector('#teamone-numgoals');
 let resetCount = document.querySelector('#num-resets');
-var denied = new Audio();
-denied.src = "https://www.myinstants.com/media/sounds/denied.mp3"
 var reset =new Audio();
 reset.src = "https://www.myinstants.com/media/sounds/avengers_.mp3"
 var cheer = new Audio()
 cheer.src = "https://www.myinstants.com/media/sounds/crowd-cheer.mp3"
-
+let playerOneShotCount = 0
+let playerOneGoalCount = 0
+let playerTwoShotCount = 0
+let playerTwoGoalCount = 0
 resetButton.addEventListener ('click', function(){
     console.log ('Score Reset, Play Again!');
 shotOne.innerHTML = 0;
@@ -26,49 +27,36 @@ resetCount.innerHTML = newCounterValue
 reset.play()
 })
 
+
      teamoneShootButton.addEventListener ('click', function(){
-    console.log ('Team One Shoots')
-    let newCounterValue = Number (shotOne.innerHTML) + 1;
-    shotOne.innerHTML = newCounterValue;
-    denied.play()
+     console.log ('Team One Shoots')
+     playerOneShotCount += 1
+     playerOneGoalCount += Math.floor(Math.random() + 0.5)
+     shotOne.innerHTML = playerOneShotCount
+     counterOne.innerHTML = playerOneGoalCount
+
+     if (playerOneGoalCount === 10){
+         alert('Team One Wins!!!!!!')
+         cheer.play()
+     }
+     
+        
     
     
-    if (shotOne.innerHTML % 1.5 === 1) {
-        let newCounterValue = Number (counterOne.innerHTML) + 1;
-        counterOne.innerHTML = newCounterValue;
-        cheer.play ()
-    
-}
-})
-
-
-
-
-
-
-
-
-
-
-
+ })
 teamTwoShotButton.addEventListener ('click', function(){
 console.log ('Team Two Shoots')
-let newCounterValue = Number (shotTwo.innerHTML) + 1;
-shotTwo.innerHTML = newCounterValue;
-denied.play()
-if (shotTwo.innerHTML % 1.5 === 1) {
-    let newCounterValue = Number (counterTwo.innerHTML) + 1;
-    counterTwo.innerHTML = newCounterValue;
-    console.log ('Team Two Scores')
+playerTwoShotCount += 1
+playerTwoGoalCount += Math.floor(Math.random() + 0.5)
+shotTwo.innerHTML = playerTwoShotCount
+counterTwo.innerHTML = playerTwoGoalCount 
+if (playerTwoGoalCount === 10){
+    alert('Team Two Wins!!!!') 
     cheer.play()
 }
-
-
-}
-
-
+ 
+ 
 
 
 
-
-)
+})
